c865d472447422291d21ffab68ab234435c47e3e
barnett <576501057@qq.com>
2018-09-08 18:52:16 +0800

    handle config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-DIRECTORY="/data"
+DIRECTORY="/app/data"
 if [ "`ls -A $DIRECTORY`" = "" ]; then
  mv /app/data2 /app/data
+fi
+
+CONFIG="/app/config"
+if [ "`ls -A $CONFIG`" = "" ]; then
+ mv /app/config2 /app/config
 fi
 exec vendor/bin/heroku-php-apache2
