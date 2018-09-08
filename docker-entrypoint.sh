d4cc02a2bcb230422bb9824ea5cf19d5510cb193
zhoujunhao <18853925545@163.com>
2018-09-08 21:17:10 +0800

    no message

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,10 @@ CONFIG="/app/config"
 if [ "`ls -A $CONFIG`" = "" ]; then
  mv /app/config2/* /app/config
 fi
+
+DZZ="/app/dzz/appmanagement"
+if [ "`ls -A $DZZ`" = "" ]; then
+ mv /app/dzz/appmanagement2/* /app/dzz/appmanagement
+fi
+
 exec vendor/bin/heroku-php-apache2
